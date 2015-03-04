e3d36df153658671942403e371d95fe3e9178ede
Thustra <peter.windey@telenet.be>
2015-03-04 18:00:04 +0100

    Refactored home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 __author__ = 'Peter'
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,20 +17,36 @@ class NewVisitorTest(unittest.TestCase):
         # Visit the homepage
         self.browser.get('http://localhost:8000')
 
-        # Title mentions TO-DO lists
+        # Title and header mention TO-DO lists
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
         # Invited to enter a TO-DO item
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
         # Enter "Buy peacock feathers"
+
+        inputbox.send_keys('Buy peacock feathers')
 
         # When hitting enter the page updates and displays
         # "1: Buy peacock feathers" as an item on the list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text inviting to add another item
         # Enter "Use peacock feathers to make fly"
-
+        self.fail('Finish the test!!')
         # Page updates again and shows an updated TO-DO list
 
         # Unique URL is generated for this user, with some explanation about it
