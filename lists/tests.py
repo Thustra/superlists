20427faf56b020a7a6489e6d90ace52dbea89403
Thustra <peter.windey@telenet.be>
2015-03-04 17:05:40 +0100

    Basic view returns html

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,24 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
+
+
+# Note: Response is an HttpResponse object and thus consists
+# of raw bytes. Hence the b
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
