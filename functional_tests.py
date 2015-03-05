2f7ecb4751f31c15360cc0b65df6ece24741bc5d
Thustra <peter.windey@telenet.be>
2015-03-05 16:28:31 +0100

    Model for list and associated migrations

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
-    
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_as_it_was(self):
         # Person opens the browseer
         # Visit the homepage
@@ -38,9 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item on the list
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text inviting to add another item
         # Enter "Use peacock feathers to make fly"
@@ -52,10 +55,8 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
         # Unique URL is generated for this user, with some explanation about it
 
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 from lists.views import home_page
 
@@ -35,3 +36,23 @@ class SmokeTest(TestCase):
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        first_item = Item()
+        first_item.text = 'Item the second'
+        first_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_item_saved = saved_items[0]
+        second_item_saved = saved_items[1]
+
+        self.assertEqual(first_item_saved.text,'The first (ever) list item')
+        self.assertEqual(second_item_saved.text, 'Item the second')
